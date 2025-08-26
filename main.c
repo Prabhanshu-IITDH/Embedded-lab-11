7e446c064342afa983f8ce4cebd868e1dce858e1
Prabhanshu-IITDH <220020045@iitdh.ac.in>
2025-08-26 17:13:16 +0530

    updated the main.c with 2nd problem's solution

--- main.c
+++ main.c
@@ -1,30 +1,43 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
-/**
- * main.c
- */
+
+void delayMs(int n);
+
 int main(void)
 {
-    SYSCTL_RCGC2_R |=   0x00000020;      // ENABLE CLOCK TO GPIOF
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;      // UNLOCK COMMIT REGISTER
-    GPIO_PORTF_CR_R   = 0x1F;            // MAKE PORTF0 CONFIGURABLE
-    GPIO_PORTF_DEN_R  = 0x1F;            // SET PORTF PINS 4 PIN
-    GPIO_PORTF_DIR_R  = 0x0E;            // SET PORTF4 PIN AS INPUT USER SWITCH PIN
-    GPIO_PORTF_PUR_R  = 0x11;            // PORTF4 IS PULLED UP
+    SYSCTL_RCGC2_R |= 0x20;            // Enable clock to Port F
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;    // Unlock PF0
+    GPIO_PORTF_CR_R   = 0x1F;          // Commit PF0�PF4
+    GPIO_PORTF_DEN_R  = 0x1F;          // Digital enable PF0�PF4
+    GPIO_PORTF_DIR_R  = 0x0E;          // PF1�PF3 output (LEDs), PF0 & PF4 input
+    GPIO_PORTF_PUR_R  = 0x11;          // Enable pull-up on PF0 & PF4
+
+    int count = 0;
+    uint32_t colors[3] = {0x02, 0x08, 0x04}; // Red, Green, Blue (PF1, PF3, PF2)
 
     while(1)
     {
-        GPIO_PORTF_DATA_R = 0x0;
-        if (!(GPIO_PORTF_DATA_R & 0x01) && (GPIO_PORTF_DATA_R & 0x10) ){
-            GPIO_PORTF_DATA_R = 0x02;
+        // Read SW2 (PF0), active low
+        if (!(GPIO_PORTF_DATA_R & 0x01))
+        {
+            delayMs(20); // debounce
+
+            if (!(GPIO_PORTF_DATA_R & 0x01)) // still pressed
+            {
+                count = (count + 1) % 3;        // cycle 0�2
+                GPIO_PORTF_DATA_R = (GPIO_PORTF_DATA_R & ~0x0E) | colors[count]; // update only LEDs
+
+                while (!(GPIO_PORTF_DATA_R & 0x01)); // wait until released
+                delayMs(20); // debounce release
+            }
         }
-        else if (!(GPIO_PORTF_DATA_R & 0x10) && (GPIO_PORTF_DATA_R & 0x01)){
-                    GPIO_PORTF_DATA_R = 0x04;
-                }
-        else if (!(GPIO_PORTF_DATA_R & 0x10) && !(GPIO_PORTF_DATA_R & 0x01)){
-                            GPIO_PORTF_DATA_R = 0x06;
-                        }
-        else GPIO_PORTF_DATA_R = 0x00;
     }
+}
+
+void delayMs(int n)
+{
+    volatile int i, j;
+    for(i = 0; i < n; i++)
+        for(j = 0; j < 3180; j++) {}   // ~1ms delay at 16 MHz
 }
